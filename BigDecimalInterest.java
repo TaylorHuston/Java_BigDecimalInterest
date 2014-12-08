5a78026df6c439eb5b05d801cd565cc333570aea
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-07 19:06:32 -0700

    First commit

---
+++ BigDecimalInterest.java
@@ -0,0 +1,25 @@
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+/*
+ * Simple program to calculate compund interest using BigDecimal
+ * From "Java How To Program, 10, Early Objects" - Chapter 8.
+ */
+public class BigDecimalInterest {
+
+    public static void main(String[] args) {
+        BigDecimal principal = BigDecimal.valueOf(1000.0);  //Initial principal amount
+        BigDecimal rate = BigDecimal.valueOf(0.05);  //Interest rate
+        BigDecimal amount;
+        
+        System.out.printf("%s%20s%n", "Year", "Amount");
+        
+        for (int i = 1; i < 11; i++) {
+            amount = principal.multiply(rate.add(BigDecimal.ONE).pow(i));
+            
+            System.out.printf("%4d%20s%n",i,NumberFormat.getCurrencyInstance().format(amount));
+        }
+    }
+    
+}
