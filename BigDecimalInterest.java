86939f99fa5c6e8e441eea7540841847903f6312
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-30 19:08:22 -0900

    Fix formatting based on checkstyle

--- BigDecimalInterest.java
+++ BigDecimalInterest.java
@@ -3,14 +3,16 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 /*
- * Simple program to calculate compund interest using BigDecimal
+ * Simple program to calculate compound interest using BigDecimal
  * From "Java How To Program, 10, Early Objects" - Chapter 8.
  */
 public class BigDecimalInterest {
 
     public static void main(String[] args) {
-        BigDecimal principal = BigDecimal.valueOf(1000.0);  //Initial principal amount
-        BigDecimal rate = BigDecimal.valueOf(0.05);  //Interest rate
+        //Initial principal amount
+        BigDecimal principal = BigDecimal.valueOf(1000.0);
+        //Interest rate
+        BigDecimal rate = BigDecimal.valueOf(0.05);  
         BigDecimal amount;
         
         System.out.printf("%s%20s%n", "Year", "Amount");
@@ -18,8 +20,9 @@ public class BigDecimalInterest {
         for (int i = 1; i < 11; i++) {
             amount = principal.multiply(rate.add(BigDecimal.ONE).pow(i));
             
-            System.out.printf("%4d%20s%n",i,NumberFormat.getCurrencyInstance().format(amount));
+            System.out.printf("%4d%20s%n", i,
+                    NumberFormat.getCurrencyInstance().format(amount));
         }
-    }
+    } //End main
     
-}
+} //End BigDecimalInterest
